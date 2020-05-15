70446e923a688506ef001e9a6a2e781f1191d980
marrodri <laza.mar64@gmail.com>
2020-05-15 17:32:49 -0500

    small change, files now reading

--- main.cpp
+++ main.cpp
@@ -3,29 +3,61 @@
 #include "stack.hpp"
 #include <string>
 #include <iostream>
+#include <vector>
+#include <fstream>
+void parser(std::vector<IOperand> stack)
+{
 
+}
+
+void command_checker()
+{
+
+}
+
+//here's where the program runs
 int main(int argc, char **argv)
 {
-	//here's where the program runs
-	
-	// initialize an array here that runs as a 
-	// stack
-	//if argc is 1; the program runs, and each execution is started
-	// by finding the ";;" as the beggining and end of line
-	
-	// if argc is higher than 1, then check the files, if the
-	// files are correctly parsed, then execute the program
-	//if the file has an exit instruction, exit the VM, if not
-	//the VM is still active
-	//if not handle the error
-
-
-	//use a while loop for the program
-	
+	// initialize an array here that runs as a stack
 	//for every new Operand created return a 
 	//pointer of the class to the new space
 	//of the stack
+	std::vector<IOperand> stack;
 
-	//also Creagt
+	// if argc is higher than 1, then check the files, if the
+	// files are correctly parsed, then execute the program
+	if (argc >= 2)
+	{
+		//if the file has an exit instruction, exit the VM, if not
+		//the VM is still active
+		std::ifstream infile;
+		infile.open(argv[1]);
+		std::cout << infile.rdbuf() << std::endl;
+		argc = 0;
+	}
+	else
+	{
+		std::cout <<  "NO FILE, GETTING FROM STDIN" << std::endl;
+		argc = 0;
+	}
+
+	//if argc is 1; the program runs, and each execution is started
+	// by finding the ";;" as the beggining and end of line
+	while (argc)
+	{
+		if(0)
+		{
+
+		}
+
+		//for each exit command, get the argc to 0;
+		if(0)
+		{
+			argc--;
+		}
+	}
+	
+	
+
 	return 0;
 }
