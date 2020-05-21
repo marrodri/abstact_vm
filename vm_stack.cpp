660c3fa31eeb547dcab1568e88cc8194315c5cdf
marrodri <laza.mar64@gmail.com>
2020-05-20 19:33:31 -0500

    checkpoint, setting the stack class

---
+++ vm_stack.cpp
@@ -0,0 +1,68 @@
+
+#include "vm_stack.hpp" 
+#include "Int8.hpp"
+vm_stack::vm_stack(vm_stack const & src)
+{
+	
+}
+
+vm_stack::~vm_stack()
+{
+}
+
+void vm_stack::call_instructions(std::vector<std::string> instruction)
+{
+	//TODO define the method that calls the instructions from the vector
+	//if the instruction needs a value check the next 
+}
+
+void vm_stack::push_value(std::string value)
+{
+
+}
+
+//ask how can we return a popped IOperand value class
+// IOperand vm_stack::pop()
+// {
+
+// }
+
+void vm_stack::dump()
+{
+	
+}
+
+void vm_stack::assert(std::string)
+{
+
+}
+
+void vm_stack::add()
+{
+
+}
+
+void vm_stack::sub()
+{
+
+}
+
+void vm_stack::mul()
+{
+
+}
+
+void vm_stack::div()
+{
+
+}
+
+void vm_stack::mod()
+{
+
+}
+
+void vm_stack::print()
+{
+	
+}
--- vm_stack.hpp
+++ vm_stack.hpp
@@ -2,7 +2,8 @@
 #define VM_STACK_HPP
 #include "IOperand.hpp"
 #include <stack>
-
+#include <string>
+#include <vector>
 class vm_stack
 {
 private:
@@ -21,37 +22,48 @@ public:
 	//push VALUE
 	//from the parameters, it will call the IOperand create* function
 	//with the value inputed, and then it will be push to the stack
+	void push_value(std::string value);
 
 	//pop function
-	//call the pop function to the private stack
+	//call the pop function to the private stack, returns the popped Ioperand value
+	IOperand pop();
 
 	//dump
+	void dump();
 	//check how it works
 
 	//assert VALUE
+	void assert(std::string value);
 
 	//add
 	//pops the top 2 values, then it makes the sum of those popped values,
 	//and the result is pushed back to the stack
+	void add();
 
 	//sub
+	void sub();
 
 	//mul
+	void mul();
 
 	//div
 	//same as previous but with division, there's an edge case to check
+	void div();
 
 	//mod
 	//there's an edge case to check for mod too
+	void mod();
 
 	//print
-	
+	void print();
+
+	void call_instructions(std::vector<std::string> instruction);
 
 	//exit(this one could be in another place)
 
 
-	//get function
-	//set function
+	//getter for the stack, like pop()??
+	//setter's are like the push value, so it should be fine
 
 };
 #endif
